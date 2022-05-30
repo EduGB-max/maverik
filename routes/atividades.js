24cb2d3a1abd2c5e72849fd4a2662592aed683bc
EduGB-max <84384908+EduGB-max@users.noreply.github.com>
2022-05-30 13:10:07 -0300

    Add files via upload

---
+++ routes/atividades.js
@@ -0,0 +1,27 @@
+
+module.exports = (app)=>{
+    app.post('/atividades', async(req,res)=>{
+
+var dados = req.body
+
+const database = require("../config/database")()
+
+const atividades = require("../models/atividades")
+   
+var gravar = await new atividades ({
+
+data:dados.data,
+tipo:dados.tipo,
+entrega:dados.entrega,
+disciplina:dados.disciplina,
+instrucoes:dados.instrucoes,
+usuarios:dados.id
+
+}).save()
+
+res.render('atividades.ejs', {nome:dados.nome,id:dados.id})
+
+})
+
+}
+
